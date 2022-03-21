3173f2eee962613350650b1c2c6caf92babf4f8b
Rastuharem <95254106+Rastuharem@users.noreply.github.com>
2022-03-21 18:06:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "Automata.h"
 
+using namespace std;
+
+int main()
+{
+    Automata DrinkMachine;
+    DrinkMachine.on();
+    cout << "Machine is on!" << '\n';
+    DrinkMachine.coin(50);
+    int drink = DrinkMachine.choice("Black tea");
+    if (DrinkMachine.check(drink)) {
+        cout << "Order accepted!"<<'\n';
+        DrinkMachine.cook(drink);
+        int change = DrinkMachine.finish();
+        cout << "Drink is ready! Please, take your change: " << change << '\n';
+    } else {
+        int change = DrinkMachine.cancel();
+        cout << "Your order is cancelled :(. Please, take your change: " << change << '\n';
+    }
+    DrinkMachine.off();
+    cout << "Machine is shutting down..." << '\n';
+    return EXIT_SUCCESS;
+}
