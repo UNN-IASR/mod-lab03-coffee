63d9c7e3707f644e65561d06f578bfa8733bcd81
Rastuharem <95254106+Rastuharem@users.noreply.github.com>
2022-03-21 18:47:56 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -15,23 +15,23 @@ enum States {
 };
 
 class Automata {
-private:
-    int cash;
-    States state;
-    const vector<string> menu =
-    { "Strawberry cocktail", "Chokolate cocktail",
-     "Black tea", "Green tea", "Coffee" };
-    const vector<int> prices = { 60, 70, 40, 45, 50 };
-public:
-    Automata();
-    void on();
-    void off();
-    void coin(int money);
-    vector<string> getMenu();
-    States getState();
-    int choice(string drink);
-    bool check(int index);
-    int cancel();
-    void cook(int index);
-    int finish();
+    private:
+        int cash;
+        States state;
+        const vector<string> menu =
+        { "Strawberry cocktail", "Chokolate cocktail",
+         "Black tea", "Green tea", "Coffee" };
+        const vector<int> prices = { 60, 70, 40, 45, 50 };
+    public:
+        Automata();
+        void on();
+        void off();
+        void coin(int money);
+        vector<string> getMenu();
+        States getState();
+        int choice(string drink);
+        bool check(int index);
+        int cancel();
+        void cook(int index);
+        int finish();
 };
