18e65131f80903fcf8c9bf6667bfd079d3cd9c40
Rastuharem <95254106+Rastuharem@users.noreply.github.com>
2022-03-21 18:06:51 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
 
+using namespace std;
+
+enum States {
+	Off,
+	Wait,
+	Accept,
+	Check,
+	Cook,
+};
+
+class Automata {
+private:
+	int cash;
+	States state;
+	const vector<string> menu = { "Strawberry cocktail", "Chokolate cocktail", "Black tea", "Green tea", "Coffee" };
+	const vector<int> prices = { 60, 70, 40, 45, 50 };
+public:
+	Automata();
+	void on();
+	void off();
+	void coin(int money);
+	vector<string> getMenu();
+	States getState();
+	int choice(string drink);
+	bool check(int index);
+	int cancel();
+	void cook(int index);
+	int finish();
+};
