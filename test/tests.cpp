c9845530b7fc7a0a6292377df807e26818b67996
Rastuharem <95254106+Rastuharem@users.noreply.github.com>
2022-03-25 18:28:25 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,44 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(taskA, test1) {
+  Automata Machine = new Automata();
+  Machine.on();
+  State res = Machine.getState();
+  EXPECT_EQ(Wait, res);
+}
+
+TEST(taskA, test2) {
+  Automata Machine = new Automata();
+  Machine.on();
+  Machine.coin(5);
+  int change = Machine.cancel();
+  EXPECT_EQ(5, change);
+}
+
+TEST(taskA, test3) {
+  Automata Machine = new Automata();
+  Machine.on();
+  Machine.coin(5);
+  int index = Machine.choice("Black tea");
+  bool res = Machine.getState();
+  EXPECT_EQ(false, res);
+}
+
+TEST(taskA, test4) {
+  Automata Machine = new Automata();
+  Machine.on();
+  Machine.coin(50000);
+  int index = Machine.choice("Black tea");
+  bool res = Machine.getState();
+  EXPECT_EQ(true, res);
+}
+
+TEST(taskA, test5) {
+  Automata Machine = new Automata();
+  Machine.on();
+  Machine.off();
+  States res = Machine.getState();
+  EXPECT_EQ(Off, res);
+}
