a04d5137e3709b0efe6dc19a8223cb353e7b449d
asv23 <asv23@live.ru>
2022-04-12 22:00:49 +0300

    Style & list fix

--- include/Automata.h
+++ include/Automata.h
@@ -1,34 +1,30 @@
+// Copyright 2022 UNN-IASR
 #pragma once
 #include <stdio.h>
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-class Automata
-{
+class Automata {
 private:
-	int cash;
-	int selection;
-	list<string> menu;
-	list<int> prices;
-	bool isChecked;
-
+  int cash;
+  int selection;	
+  std::string menu[4];
+  int prices[4];
+  bool isChecked;
 public:
-	enum States {
-		OFF, WAIT, ACCEPT, CHECK, COOK, FINISH
-	};
-
-	Automata();
-
-	void on();
-	void off();
-	void coin(int);
-	void getMenu();
-	States getState();
-	void choice(int);
-	void check();
-	void cancel();
-	void cook();
-	void finish();
+  enum States {
+    OFF, WAIT, ACCEPT, CHECK, COOK, FINISH
+  };
+  States state;
+  Automata();
+  void on();
+  void off();
+  void coin(int);
+  void getMenu();
+  States getState();
+  void choice(int);
+  void check();
+  void cancel();
+  void cook();
+  void finish();
 };
--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,78 +1,72 @@
+// Copyright 2022 UNN-IASR
 #include "Automata.h"
 
 Automata::Automata() {
-	States state = off;
-	cash = 0;
-	selection = -1;
-
-	menu->add("Кофе");
-	menu->add("Горячий кофе");
-	menu->add("Очень горячий кофе");
-	menu->add("Кофесодержащий продукт");
-
-	prices->add(100);
-	prices->add(110);
-	prices->add(90);
-	prices->add(250);
-
-	isChecked = false
+  state = OFF;
+  cash = 0;
+  selection = -1;
+  menu[0] = "Кофе";
+  menu[1] = "Горячий кофе";
+  menu[2] = "Очень горячий кофе";
+  menu[3] = "Кофесодержащий продукт";
+  prices[0] = 100;
+  prices[1] = 110;
+  prices[2] = 90;
+  prices[3] = 250;
+  isChecked = false;
 }
 void Automata::on() {
-	state = wait;
+  state = WAIT;
 }
 void Automata::off() {
-	state = OFF;
+  state = OFF;
 }
 void Automata::coin(int sum) {
-	cash += sum;
-	state = ACCEPT;
+  cash += sum;
+  state = ACCEPT;
 }
 void Automata::getMenu() {
-	for (int i = 0; i < menu.size(); i++)
-	{
-		cout << menu[i] << " " << prices[i] << '\n';
-	}
+  for (int i = 0; i < 4; i++)	{
+    std::cout << menu[i] << " " << prices[i] << '\n';
+  }
 }
-States Automata::getState() {
-	return state();
+Automata::States Automata::getState() {
+  return state;
 }
 void Automata::choice(int num) {
-	selection = num;
-	state = CHECK;
+  selection = num;
+  state = CHECK;
 }
 void Automata::check() {
-	if (state != ACCEPT) {
-		state = WAIT;
-		return;
-	}
-	if (cash < prices[selection]) {
-		state = WAIT;
-	}
-	else {
-		isChecked = true;
-	}
+  if (state != ACCEPT) {
+    state = WAIT;
+    return;
+  }
+  if (cash < prices[selection]) {
+    state = WAIT;
+  }
+  else {
+    isChecked = true;
+  }
 }
 void Automata::cancel() {
-	if (state != OFF) {
-		isChecked = false;
-		state = WAIT;
-		cash = 0;
-	}
+  if (state != OFF) {
+    isChecked = false;
+    state = WAIT;
+    cash = 0;
+  }
 }
-
 void Automata::cook() {
-	if (isChecked) {
-		cash -= prices[selection];
-		state == COOK
-	}
-	else {
-		state = WAIT;
-	}
+  if (isChecked) {
+    cash -= prices[selection];
+    state == COOK;
+  } else {
+    state = WAIT;
+  }
 }
-
 void Automata::finish() {
-	if (state == COOK) {
-		state = WAIT;
-		cash = 0;
-	}
+  if (state == COOK) {
+    state = WAIT;
+    cash = 0;
+  }
 }
