5f91538496df699f29a750aeb3382c59e0eaa186
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-04 16:04:36 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,69 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task1, test1) {
+    Automata Automat1;
+    Automat1.on();
+    STATES result = Automat1.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task1, test2) {
+    Automata Automat1;
+    Automat1.on();
+    Automat1.coin(170);
+    STATES result = Automat1.getState();
+    EXPECT_EQ(ACCEPT, result);
+}
+
+TEST(task1, test3) {
+    Automata Automat1;
+    Automat1.on();
+    Automat1.coin(170);
+    Automat1.choice(1);
+    STATES result = Automat1.getState();
+    EXPECT_EQ(CHECK, result);
+}
+
+TEST(task1, test4) {
+    Automata Automat1;
+    Automat1.on();
+    Automat1.coin(170);
+    Automat1.choice(1);
+    Automat1.cook();
+    STATES result = Automat1.getState();
+    EXPECT_EQ(COOK, result);
+}
+
+TEST(task1, test5) {
+    Automata Automat1;
+    Automat1.on();
+    Automat1.coin(170);
+    Automat1.choice(1);
+    Automat1.cook();
+    Automat1.finish();
+    STATES result = Automat1.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task1, test6) {
+    Automata Automat1;
+    Automat1.on();
+    Automat1.coin(170);
+    Automat1.choice(1);
+    Automat1.cook();
+    Automat1.finish();
+    Automat1.off();
+    STATES result = Automat1.getState();
+    EXPECT_EQ(OFF, result);
+  
+TEST(task2, test1) {
+    Automata Automat1;
+    Automat1.on();
+    Automat1.coin(170);
+    Automat1.choice(1);
+    Automat1.cancel();
+    STATES result = Automat1.getState();
+    EXPECT_EQ(WAIT, result);
+}
