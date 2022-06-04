4037c16056686897f9c6b4305d9bf68d91001fb8
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-04 16:18:34 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,3 +1,5 @@
+// Copyright 2022 UNN-IASR
+
 #pragma once
 #include <iostream>
 #include <string>
@@ -7,23 +9,23 @@ using std::string;
 enum STATES { COOK, CHECK, ACCEPT, WAIT, OFF };
 
 class Automata {
-  public:
-    Automata();
-    void on();
-    void off();
-    void coin(const int);
-    void getMenu();
-    STATES getState();
-    void choice(const int);
-    void check();
-    void cancel();
-    void cook();
-    void finish();
+ public:
+  Automata();
+  void on();
+  void off();
+  void coin(const int);
+  void getMenu();
+  STATES getState();
+  void choice(const int);
+  void check();
+  void cancel();
+  void cook();
+  void finish();
   
-  private:
-    int cash;
-    string* menu;
-    int* prices;
-    STATES state;
-    int option;
+ private:
+  int cash;
+  string* menu;
+  int* prices;
+  STATES state;
+  int option;
 };
