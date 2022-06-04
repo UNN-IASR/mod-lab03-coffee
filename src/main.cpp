a4cd67b2dcf3af87dc360d0509bf9c775f27bcbc
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-04 15:40:04 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Automata.h"
+
+int main() {
+    Automata Automat1;
+
+    Automat1.on();
+    Automat1.coin(70);
+    Automat1.choice(3);
+    Automat1.check();
+    Automat1.cook();
+    Automat1.finish();
+    Automat1.off();
+}
