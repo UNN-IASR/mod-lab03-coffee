81ba0237851331f51717ac0192460da009c20627
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-03-23 19:09:49 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -4,22 +4,22 @@
 #include <vector>
 
 Automata::Automata() {
-	state = state_off;
-	cash = 0;
+    state = state_off;
+    cash = 0;
 }
 
 Automata::Automata(states st) {
-	state = st;
-	cash = 0;
+    state = st;
+    cash = 0;
 }
 
 void Automata::on() {
-	if (state == state_off)
-		state = state_on;
-	else
-		state = state;
+    if (state == state_off)
+        state = state_on;
+    else 
+        state = state;
 }
 
 states Automata::getState() {
-	return state;
+    return state;
 }
