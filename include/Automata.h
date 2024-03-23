ec5810345b758e45db7f21ef5db943d25d1fcbae
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-03-23 16:31:44 +0300

    Update and rename Automata.t to Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+typedef int money;
+using namespace std;
+
+enum states {
+    state_on, state_off, state_cook, state_wait, state_accept, state_check, state_error, state_finish    
+};
+
+class Automata {
+private:
+    int cash;
+    std::vector <std::string> menu;
+    std::vector <int> prices;
+    states state;    
+
+public:
+
+    explicit Automata();
+    explicit Automata(states st);
+    void on();
+    void off();
+    void coin(money d);
+    void setMenu(const string&);
+    states getState();
+    bool choice(string);
+    bool check(const string&);
+    void cancel();
+    void cook();
+    void finish();
+};
