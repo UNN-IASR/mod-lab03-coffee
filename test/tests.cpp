8b5bcb43a8841f8a6f4ef18c07944daae0e5b7a7
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-03-23 16:32:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,18 @@
 // Copyright 2022 GHA Test Team
 
 #include <gtest/gtest.h>
-#include "Automata.h"
+#include "Automata.h
+
+TEST(fun_on, run) {
+  Automata a;
+  a.on();
+  states st = a.getState();
+  EXPECT_EQ(state_on, st);
+}
+
+TEST(fun_on, ignore) {
+	Automata a(state_cook);
+	a.on();
+	states st = a.getState();
+	EXPECT_EQ(state_cook, st);
+}
