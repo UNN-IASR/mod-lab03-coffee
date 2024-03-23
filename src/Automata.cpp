ed2faed738e296422651ddfe04b7af426f86ad7a
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-03-23 16:33:02 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,1 +1,25 @@
 
+#include "Automata.h"
+#include <string>
+#include <vector>
+
+
+Automata::Automata() {
+	state = state_off;
+	cash = 0;
+}
+
+Automata::Automata(states st) {
+	state = state_off;
+	cash = 0;
+}
+void Automata::on() {
+	if (state == state_off)
+		state = state_on;
+	else state = state;
+}
+
+
+states Automata::getState() {
+	return state;
+}
