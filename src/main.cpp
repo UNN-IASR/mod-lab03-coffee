27add3e39e5ba6f4323116404a8b4b747d3c34f8
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2024-03-23 16:33:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
+// 3.CoffeMashina.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Automata.h"
+
+int main()
+{
+    Automata a;
+    a.on();
+    states st = a.getState();
+    
+}
+
 
