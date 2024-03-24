2b470552e98b58db5e207e21a31310f2ae6600f0
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2024-03-24 16:12:57 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Automata.h"
+
+int main() {
+    Automata CoffeeBreak;
+    //Включаем автомат, покупаем напиток
+    CoffeeBreak.on();
+    CoffeeBreak.getMenu();
+    CoffeeBreak.coin(30);
+    CoffeeBreak.choice(3);
+    std::cout << std::endl;
+
+    //Докидываем нехватающие монеты
+    CoffeeBreak.coin(1);
+    CoffeeBreak.choice(4);
+    CoffeeBreak.coin(39);
+    CoffeeBreak.choice(4);
+    std::cout << std::endl;
+
+    //Отмена выбора
+    std::cout << std::endl;
+    CoffeeBreak.coin(100);
+    CoffeeBreak.cancel();
+    std::cout << CoffeeBreak.getState() << std::endl;
+
+
+    CoffeeBreak.off();
+}
