1f8ffebb89eb860b87d57aa541a9cb616dc186ce
Mazniker Anton <92749509+ImperialSpaceExplorer@users.noreply.github.com>
2024-03-24 18:22:53 +0300

    Update Automata.h - test update

--- include/Automata.h
+++ include/Automata.h
@@ -1,36 +1,34 @@
+// Copyright 2022 GHA Test Team
+
 #pragma once
 #include <iostream>
-using namespace std;
+#include <string>
 
 enum States {
-		Off,
-		Wait,
-		Accept,
-		Check,
-		Cook
-	};
+Off,
+Wait,
+Accept,
+Check,
+Cook
+};
 
-class Automata
-{
+class Automata{
+ public:
+void on();
+void off();
+void coin(unsigned int sum);
+void GetMenu();
+States GetState();
+void choice(int num);
+void check(int num);
+void cancel();
+void cook(int num);
+void finish();
+Automata();
 
-public:
-	
-	void on();
-	void off();
-	void coin(unsigned int sum);
-	void GetMenu();
-	States GetState();
-	void choice(int num);
-	void check(int num);
-	void cancel();
-	void cook(int num);
-	void finish();
-	Automata();
-
-private:
-	unsigned int cash;
-	string* menu;
-	unsigned int* prices;
-	States state;
-
+ private:
+unsigned int cash;
+std::string* menu;
+unsigned int* prices;
+States state;
 };
