9d7f8fbbfef261e7842a91809d669851b1524c8a
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2024-03-24 16:28:47 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -14,12 +14,15 @@ private:
     std::vector<std::string> menu;
     std::vector<int> prices;
     STATES state;
+
     bool check(int);
     void cook(int);
     void finish(int);
+
 public:
     Automata();
     ~Automata();
+
     void on();
     void off();
     void coin(int);
@@ -28,4 +31,5 @@ public:
     void choice(int);
     void cancel();
 };
+
 #endif  // INCLUDE_AUTOMATA_H_
