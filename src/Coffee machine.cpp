1806092db585be7ffbab6d33acc3b000b236f966
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-03-25 13:27:22 +0300

    Add files via upload

---
+++ src/Coffee machine.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Automata.h"
+
+int main()
+{
+    Automata Machine;
+
+    Machine.on();
+    Machine.getMenu();
+    Machine.coin(50);
+    Machine.choice("cacao");
+
+    Machine.off();
+}
