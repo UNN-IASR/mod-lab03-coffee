6da75f0c149138dfc209dc286c68728398f771c7
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-03-25 16:51:06 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,30 @@
+#ifndef Automata.h
+#define AUTOMATA.h
+#include <list>
+#include <map>
+#include <string>
+
+enum states {Off, Wait, Accept, Check, Cook};
+
+class Automata {
+private:
+	int cash;
+	std::map<std::string, int> prices;
+	states state;
+public:
+	Automata();
+	~Automata();
+
+	void on();
+	void off();
+	void coin(int money);
+	void getMenu();
+	states getState();
+	void choice(std::string name);
+	bool check(std::string name);
+	void cancel();
+	void cook(std::string name);
+	void finish(std::string name);
+};
+
+#endif
