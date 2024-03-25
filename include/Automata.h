cf1f37bed6d925a6f06513b1a53265f352425922
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-03-25 17:48:18 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -9,21 +9,21 @@ enum states {Off, Wait, Accept, Check, Cook};
 class Automata {
  private:
     int cash;
-	std::map<std::string, int> prices;
-	states state;
+    std::map<std::string, int> prices;
+    states state;
  public:
-	Automata();
-	~Automata();
-	void on();
-	void off();
-	void coin(int money);
-	void getMenu();
-	states getState();
-	void choice(std::string name);
-	bool check(std::string name);
-	void cancel();
-	void cook(std::string name);
-	void finish(std::string name);
+    Automata();
+    ~Automata();
+    void on();
+    void off();
+    void coin(int money);
+    void getMenu();
+    states getState();
+    void choice(std::string name);
+    bool check(std::string name);
+    void cancel();
+    void cook(std::string name);
+    void finish(std::string name);
 };
 
 #endif  // INCLUDE_AUTOMATA_H_
