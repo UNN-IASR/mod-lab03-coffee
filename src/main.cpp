ea6445faf04959bd7a120ce4c76a76a4e81b39ea
Екатерина <140340116+RuzKate@users.noreply.github.com>
2024-03-27 16:03:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,28 @@
 #include "../include/Automata.h"
 
 int main() {
-	std::vector <std::string> states = { "OFF", "WAIT", "ACCEPT", "CHECK", "COOK" };
-	Automata a;
+    std::vector <std::string> states = { "OFF", "WAIT", "ACCEPT", "CHECK", "COOK" };
+    Automata a;
 
-	a.on();
-	std::cout << states[a.getState()] << std::endl;
-	a.coin(50);
-	a.coin(100);
-	std::cout << a.getCash() << std::endl;
-	std::cout << states[a.getState()] << std::endl;
-	a.cancel();
+    a.on();
+    std::cout << states[a.getState()] << std::endl;
+    a.coin(50);
+    a.coin(100);
+    std::cout << a.getCash() << std::endl;
+    std::cout << states[a.getState()] << std::endl;
+    a.cancel();
 
-	std::cout << states[a.getState()] << std::endl;
-	a.coin(150);
-	a.getMenu();
-	a.choice(2);
-	std::cout << states[a.getState()] << std::endl;
-	std::cout << a.getCash() << std::endl;
+    std::cout << states[a.getState()] << std::endl;
+    a.coin(150);
+    a.getMenu();
+    a.choice(2);
+    std::cout << states[a.getState()] << std::endl;
+    std::cout << a.getCash() << std::endl;
 
-	a.coin(50);
-	a.choice(3);
-	std::cout << states[a.getState()] << std::endl;
-	a.coin(100);
-	a.choice(3);
-	std::cout << states[a.getState()] << std::endl;
+    a.coin(50);
+    a.choice(3);
+    std::cout << states[a.getState()] << std::endl;
+    a.coin(100);
+    a.choice(3);
+    std::cout << states[a.getState()] << std::endl;
 }	
