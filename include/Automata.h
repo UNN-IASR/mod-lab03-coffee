4ffc1e2b1dcc4cd02be90ed9865cbf69fb1c1127
Ekaterina <katya_ryzhevskaya@mail.ru>
2024-03-27 15:38:44 +0300

    Рыжевская Екатерина Пипр

---
+++ include/Automata.h
@@ -0,0 +1,38 @@
+// Copyright 2024 Ekaterina Ryzhevskaya
+
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
+
+#include <vector>
+#include <string>
+
+enum STATES
+{
+	OFF, WAIT, ACCEPT, CHECK, COOK
+};
+
+class Automata
+{
+private:
+	int cash;
+	std::vector <std::string> menu;
+	std::vector <int> prices;
+	STATES state;
+
+	void cook();
+	void finish();
+	void check(int);
+public:
+	Automata();
+
+	void on();
+	void off();
+	void coin(int);
+	void getMenu();
+	STATES getState();
+	void choice(int);
+	void cancel();
+	int getCash();
+};
+
+#endif  // INCLUDE_AUTOMATA_H_
---
+++ src/Automata.cpp
@@ -0,0 +1,80 @@
+// Copyright 2024 Ekaterina Ryzhevskaya
+
+#include <iostream>
+#include "../include/Automata.h"
+#include <windows.h>
+
+Automata::Automata() {
+	state = OFF;
+	cash = 0;
+
+	menu = { "Tea", "Americano", "Cappucino", "Latte", "Cocoa", "Water" };
+	prices = { 80, 100, 150, 150, 130, 50 };
+}
+
+int Automata::getCash() {
+	return cash;
+}
+
+void Automata::on() {
+	if (state == OFF) state = WAIT;
+}
+
+void Automata::off() {
+	if (state == WAIT) state = OFF;
+}
+
+void Automata::coin(int sum) {
+	if (state == WAIT || state == ACCEPT) {
+		state = ACCEPT;
+		cash += sum;
+	}
+}
+
+void Automata::getMenu() {
+	if (state == WAIT || state == ACCEPT) {
+		std::cout << "MENU:" << std::endl;
+		for (int i = 0; i < menu.size(); i++) {
+			std::cout << i + 1 << ". " << menu[i] << " - " << prices[i] <<std::endl;
+		}
+	}
+}
+
+STATES Automata::getState() {
+	return state;
+}
+
+void Automata::choice(int num) {
+	if (state == ACCEPT && num >= 1 && num <= menu.size()) {
+		state = CHECK;
+		check(num);
+	}
+}
+
+void Automata::check(int num) {
+	if (cash >= prices[num - 1]) {
+		cash -= prices[num - 1];
+		cook();
+	}
+	else {
+		state = ACCEPT;
+	}
+}
+
+void Automata::cancel() {
+	if (state == ACCEPT) {
+		cash = 0;
+		state = WAIT;
+	}
+}
+
+void Automata::cook() {
+	state = COOK;
+	Sleep(1000);
+	finish();
+}
+
+void Automata::finish() { // отдал сдачу
+	cash = 0;
+	state = WAIT;
+}
