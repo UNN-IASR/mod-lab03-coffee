2ca97c34c8b6feeadc0484cfb23e9fea31f18aed
Mikhail <106872530+Mikhas3009@users.noreply.github.com>
2024-03-28 13:14:22 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <stdexcept>
 
+struct MenuItem {
+    float price;
+    std::string name;
+};
+
+class Automata {
+ private:
+    enum States {
+        OFF = 1,
+        WAIT = 2,
+        ACCEPT = 3,
+        CHECK = 4,
+        COOK = 5 
+    };
+    std::map<int, MenuItem> coffeMenuMap;
+    States currentState;
+    float currentCash;
+    MenuItem item;
+
+ public:
+    Automata();
+
+    void on();
+    
+    void off();
+    
+    void coin(float value);
+    
+    void choice(float value);
+    
+    bool check();
+    
+    void cancel();
+    
+    void cook();
+    
+    void finish();
+    
+    void getMenu();
+    
+    int getAutomataState();
+    
+    float getCurrentCash();
+    
+    MenuItem getChoice();
+    
+};
