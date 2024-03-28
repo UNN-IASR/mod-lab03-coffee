4407ab2d3df1609539073d8e31063fbfc6778c88
Mikhail <106872530+Mikhas3009@users.noreply.github.com>
2024-03-28 19:44:53 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -20,8 +20,7 @@ class Automata {
         WAIT = 2,
         ACCEPT = 3,
         CHECK = 4,
-        COOK = 5 
-    };
+        COOK = 5 };
     std::map<int, MenuItem> coffeMenuMap;
     States currentState;
     float currentCash;
@@ -41,8 +40,7 @@ class Automata {
     void getMenu();
     int getAutomataState();
     float getCurrentCash();
-    MenuItem getChoice(); 
-};
+    MenuItem getChoice(); };
 class Logger{
 
   public:
